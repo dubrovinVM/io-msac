9f0aff35122203dbb75da3bfe1ec908021e15738
vites86@gmail.com <viktor.dubrovin@ukr.net>
2020-11-29 13:44:32 +0200

    Add IO to title.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta property="og:title" content="MSAC" />
     <meta name="description" content="MILITARY AND SPORTS ALL-ROUND COMPETITION">
     <link rel="shortcut icon" type="image/x-icon" href="../img/favicon.ico">
-    <title>MSAC</title>   
+    <title>IO MSAC</title>   
 </head>
 
 <body data-spy="scroll" data-target=".navbar">

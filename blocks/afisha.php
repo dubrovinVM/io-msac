652afb84f6bc062c8ec9b0e31dc3acf0f05970e6
vites86@gmail.com <viktor.dubrovin@ukr.net>
2020-12-01 21:00:47 +0200

    Add online info.

--- blocks/afisha.php
+++ blocks/afisha.php
@@ -9,12 +9,12 @@
                         </div>
 
                         <div class="row" style="margin-top:20px;margin-bottom:20px;">    
-                            <!-- <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">                       
-                                 <a href="./files/2020/afisha_world.jpg" target="_blank">
-                                     <img src="./files/2020/afisha_world.jpg" width="60%">
+                            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">                       
+                                 <a href="./img/2020/afisha_online_2020.jpg" target="_blank">
+                                     <img src="./img/2020/afisha_online_2020.jpg" width="60%">
                                  </a>
                             </div>
-                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">                       
+                            <!--<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">                       
                                  <a href="./files/2020/afisha_world2.jpg" target="_blank">
                                      <img src="./files/2020/afisha_world2.jpg" width="60%">
                                  </a>
@@ -23,21 +23,21 @@
                     </div>  
                     <hr style="color:white !important;">
 
-                    <!-- <div class="row" style="margin-top:20px;">
+                    <div class="row" style="margin-top:20px;">
                           <div class="col-lg-4">
-                              <p>Detailed information<br>World Championship</p>
-                              <a href="./files/2020/world_reglament_2020_en.pdf" target="_blank" class="btn btn-default btn-lg">Download Document Eng</a>
+                              <p>Detailed information<br>World Online Championship</p>
+                              <a href="./files/2020/Reglament-Online-MSAC 04-06.12.2020 eng.pdf" target="_blank" class="btn btn-default btn-lg">Download Document Eng</a>
                           </div>
                           <div class="col-lg-4">
-                              <p>Детальна інформація<br>Чемпіонат Світу</p>
-                              <a href="./files/2020//world_reglament_2020_uk.pdf" target="_blank" class="btn btn-default btn-lg">Загрузити документ</a>
+                              <p>Детальная информация<br>Чемпионат мира онлайн</p>
+                              <a href="./files/2020/Reglament-Online-MSAC 04-06.12.2020 rus.pdf" target="_blank" class="btn btn-default btn-lg">Загрузить документ</a>
                           </div>
                           <div class="col-lg-4">
-                              <p>Детальна інформація<br>Міжнародний турнір</p>
-                              <a href="./files/2020/international_competition_reglament_2020_uk.pdf" target="_blank" class="btn btn-default btn-lg">Загрузити документ</a>
+                              <p>Des informations détaillées<br>Tournoi international</p>
+                              <a href="./files/2020/APPROUVÉ MSAC 04-06.12.2020 fr.pdf" target="_blank" class="btn btn-default btn-lg">Téléchargez le document</a>
                           </div>
                     </div>                   
-                    <hr style="color:white !important;"> -->
+                    <hr style="color:white !important;">
                     <div class="row" style="margin-top:40px; text-align:center">
                           <div>
                               <p>Rules<br> Military and Sports<br>All-Round Competitions (MSAC)</p>
